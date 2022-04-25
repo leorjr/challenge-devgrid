4c9aa2b841cb69b1c72f334233d1ba34884d08ed
Leonardo Rodrigues <leonardorodrigues@MacBook-Air-de-Leonardo.local>
2022-04-25 14:45:06 -0300

    fix: adjustments on type to params from dotenv

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import requests
 load_dotenv()
 
 cache = TTLCache(
-    maxsize=os.environ['DEFAULT_MAX_NUMBER'], ttl=os.environ['CACHE_TTL'])
+    maxsize=int(os.environ['DEFAULT_MAX_NUMBER']), ttl=float(os.environ['CACHE_TTL']))
 
 app = Flask(__name__)
 
@@ -15,7 +15,6 @@ app = Flask(__name__)
 @app.route('/temperature/<city>')
 @cached(cache)
 def home(city):
-
     link_request = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={os.environ['API_KEY']}"
     response = requests.get(link_request).json()
 
